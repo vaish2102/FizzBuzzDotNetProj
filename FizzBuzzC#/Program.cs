f8495ca8664d294fd6b27d07432209c2e57aa897
Vaishnavi Rajasekar <vaishnavi.rajasekar@softwire.com>
2025-02-24 11:57:48 +0000

    Print 1 to 100

--- FizzBuzzC#/Program.cs
+++ FizzBuzzC#/Program.cs
@@ -3,7 +3,20 @@
 namespace FizzBuzz {
     public class FizzBuzzNew {
         public static void Main(String[] args){
+            //Part 1 - Hello World
+            FizzBuzzNew.PrintMessage();
+            //Part 2 - Print 1 to 100
+            FizzBuzzNew.PrintNumbers();
+        }
+
+        public static void PrintMessage() {
             Console.WriteLine("Hello, World!");
         }
+        public static void PrintNumbers() {
+            for(var num = 1 ; num <101; num++){
+                Console.WriteLine(num);
+            }
+        }
+        
     }
 }    
