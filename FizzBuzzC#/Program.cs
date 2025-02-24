f9b13d166bb093ee56a705c6c5545501d9816d63
Vaishnavi Rajasekar <vaishnavi.rajasekar@softwire.com>
2025-02-24 11:47:17 +0000

    Print Hello World

---
+++ FizzBuzzC#/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace FizzBuzz {
+    public class FizzBuzzNew {
+        public static void Main(String[] args){
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}    
